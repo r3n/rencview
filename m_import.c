ba4443ad226195c14229fddf5f6fe1a4837ffb3d
iArnold <arnoldvanh@gmail.com>
2018-06-01 19:09:22 +0000

    Create m_import.c
    A C file???

---
+++ m_import.c
@@ -0,0 +1,12 @@
+/*
+**********************
+* Includes for OpenGL
+**********************
+*/
+#include <stdlib.h>
+#include <GL/glew.h>
+#ifdef __APPLE__
+#  include <GLUT/glut.h>
+#else
+#  include <GL/glut.h>
+#endif
